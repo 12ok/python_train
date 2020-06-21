dde3ce406bd2cd3261f4662b298cddcee68cc93b
Olga Krasavina <krasavina.olga.12.12@gmail.com>
2020-06-21 22:31:18 +0300

    Fix Logout and refactor method for group

--- fixture/group.py
+++ fixture/group.py
@@ -34,9 +34,7 @@ class GroupHelper:
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_form(group)
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
 
