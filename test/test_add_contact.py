ff00d21dc1fe31d375eae1fba5f19d4c0f9a8902
Olga Krasavina <krasavina.olga.12.12@gmail.com>
2020-07-04 23:12:20 +0300

    add assert for test add modify contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -6,4 +6,9 @@ def test_add_contact(app):
     contact = Contact("Oleg", "Petrovich", "Ivanov", "opi", "Mr", "The name", "street 10", "8121234556",
                       "9112223355", "8127418523", "123", "mailone@rr.rr", "mailtwo@tt.tt",
                       "mailthree@yy.yy", "www", "city street", "12", "none")
+    old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,12 +4,26 @@ from model.contact import Contact
 def test_modify_first_contact_name(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="Name", middlename="Middle", lastname="Surname"))
-    app.contact.modify_first_contact(
-        Contact(firstname="Anna", middlename="Sergeevna", lastname="Makarova", nik="anna", title="Ms"))
+    contact = Contact(firstname="Anna", middlename="Sergeevna", lastname="Makarova", nik="anna", title="Ms")
+    old_contacts = app.contact.get_contact_list()
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_modify_first_contact_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="Name", middlename="Middle", lastname="Surname"))
-    app.contact.modify_first_contact(
-        Contact(home="+7(812)852-12-52", mobile="+7(911)852-96-96", work="+7(495)123-52-52", fax="999"))
+    contact = Contact(home="+7(812)852-12-52", mobile="+7(911)852-96-96", work="+7(495)123-52-52", fax="999")
+    old_contacts = app.contact.get_contact_list()
+    contact.id = old_contacts[0].id
+    contact.lastname = old_contacts[0].lastname
+    contact.firstname = old_contacts[0].firstname
+    app.contact.modify_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
