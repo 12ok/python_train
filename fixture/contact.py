171ab567684dc3d56790fb1c7a54e70390ebf7b4
Olga Krasavina <krasavina.olga.12.12@gmail.com>
2020-07-12 14:53:54 +0300

    Add test phones

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,5 @@
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -66,12 +67,21 @@ class ContactHelper:
 
     def modify_contact_by_index(self, contact, index):
         wd = self.app.wd
-        self.open_home_page()
-        wd.find_elements_by_css_selector("[title='Edit']")[index].click()
+        self.open_contact_to_edit_by_index(index)
         self.fill_contact_form(contact)
         wd.find_element_by_name("update").click()
         self.return_to_home_page()
         self.contact_cache = None
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        wd.find_elements_by_css_selector("[title='Edit']")[index].click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        wd.find_elements_by_css_selector("[title='Details']")[index].click()
 
     def save_form(self):
         wd = self.app.wd
@@ -93,5 +103,30 @@ class ContactHelper:
                 id = element.find_element_by_name("selected[]").get_attribute("value")
                 lastname = element.find_element_by_css_selector("td:nth-child(2)").text
                 name = element.find_element_by_css_selector("td:nth-child(3)").text
-                self.contact_cache.append(Contact(firstname=name, lastname=lastname, id=id))
+                all_contacts = element.find_element_by_css_selector("td:nth-child(6)").text
+                self.contact_cache.append(
+                    Contact(firstname=name, lastname=lastname, id=id, all_phones_from_home_page=all_contacts))
         return list(self.contact_cache)
+
+    def get_contact_info_from_edit_page(self, index):
+        self.open_contact_to_edit_by_index(index)
+        wd = self.app.wd
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        home = wd.find_element_by_name("home").get_attribute("value")
+        mobile = wd.find_element_by_name("mobile").get_attribute("value")
+        work = wd.find_element_by_name("work").get_attribute("value")
+        phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, home=home, mobile=mobile, work=work, phone2=phone2,
+                       id=id)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home = re.search("H: (.*)", text).group(1)
+        mobile = re.search("M: (.*)", text).group(1)
+        work = re.search("W: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Contact(home=home, mobile=mobile, work=work, phone2=phone2)
--- model/contact.py
+++ model/contact.py
@@ -5,7 +5,8 @@ class Contact:
 
     def __init__(self, firstname=None, middlename=None, lastname=None, nik=None, title=None, company=None, address=None,
                  home=None, mobile=None, work=None, fax=None, email=None,
-                 email2=None, email3=None, page=None, address2=None, phone2=None, notes=None, id=None):
+                 email2=None, email3=None, page=None, address2=None, phone2=None, notes=None, id=None,
+                 all_phones_from_home_page=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -25,13 +26,14 @@ class Contact:
         self.phone2 = phone2
         self.notes = notes
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
         return "%s:%s %s" % (self.id, self.lastname, self.firstname)
 
     def __eq__(self, other):
         return (
-                           self.id is None or other.id is None or self.id == other.id) and self.lastname == other.lastname and self.firstname == other.firstname
+                       self.id is None or other.id is None or self.id == other.id) and self.lastname == other.lastname and self.firstname == other.firstname
 
     def id_or_max(self):
         if self.id:
